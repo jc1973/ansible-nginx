c4b8118332d1f38d3a40d6c50af58088467a463b
John Carter <john.a.carter@capgemini.com>
2017-11-16 16:34:15 +0000

    get tests running under docker

--- test/integration/default/inspec/default_spec.rb
+++ test/integration/default/inspec/default_spec.rb
@@ -1,9 +1,8 @@
-describe port(80) do
-  it { should be_listening }
-  #its('processes') {should include 'syslog'}
-end
-
 describe command('curl localhost') do
   its('stdout') { should match /elcome/ }
 end
 
+describe port(80) do
+  it { should be_listening }
+  #its('processes') {should include 'syslog'}
+end
