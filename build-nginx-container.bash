5d56bf976a7757aec682fd8715f41180ae42e46d
John Carter <john.a.carter@capgemini.com>
2017-11-21 12:06:50 +0000

    test image

--- build-nginx-container.bash
+++ build-nginx-container.bash
@@ -5,4 +5,5 @@ virtualenv ansible-container
 eval "$(chef shell-init bash)"
 ansible-container build
 ansible-container run
-
+DOCKER_IMAGE=$(docker ps 2>/dev/null | grep nginx | awk '{print $1}')
+inspec exec test/integration/default/inspec/default_spec.rb -t docker://${DOCKER_IMAGE}
