100f6f5a3233b35923a25481439ac3ccd147c978
John Carter <john.a.carter@capgemini.com>
2017-11-21 13:20:28 +0000

    give user feedback

--- build-nginx-container.bash
+++ build-nginx-container.bash
@@ -1,9 +1,31 @@
 #!/bin/bash 
 
-virtualenv ansible-container
-. /var/lib/jenkins/ansible-container/bin/activate
-eval "$(chef shell-init bash)"
-ansible-container build
-ansible-container run
-DOCKER_IMAGE=$(docker ps 2>/dev/null | grep nginx | awk '{print $1}')
-inspec exec test/integration/default/inspec/default_spec.rb -t docker://${DOCKER_IMAGE}
+STATUS=0
+
+. /var/lib/jenkins/ansible-container/bin/activate || STATUS=1
+[ $STATUS  -eq 1 ] && echo cannot set up python virtual environment
+[ $STATUS  -eq 1 ] && exit $STATUS
+
+ansible-container build || STATUS=1
+[ $STATUS  -eq 1 ] && echo cannot build container with ansbile-container build
+[ $STATUS  -eq 1 ] && exit $STATUS
+
+ansible-container run || STATUS=1
+[ $STATUS  -eq 1 ] && echo cannot run container with ansbile-container run
+[ $STATUS  -eq 1 ] && exit $STATUS
+
+DOCKER_IMAGE=$(docker ps 2>/dev/null | grep nginx | awk '{print $1}') || STATUS=1
+[ $STATUS  -eq 1 ] && echo cannot get docker image id 
+[ $STATUS  -eq 1 ] && exit $STATUS
+
+eval "$(chef shell-init bash)" || STATUS=1
+[ $STATUS  -eq 1 ] && echo cannot set up test environment
+[ $STATUS  -eq 1 ] && exit $STATUS
+
+inspec exec test/integration/default/inspec/default_spec.rb -t docker://${DOCKER_IMAGE} || STATUS=1
+[ $STATUS  -eq 1 ] && echo tests failed
+[ $STATUS  -eq 1 ] && exit $STATUS
+
+ansible-container destroy || STATUS=1
+[ $STATUS  -eq 1 ] && echo cannot destroy container with ansbile-container destroy
+exit ${STATUS}
