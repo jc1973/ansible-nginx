cb8c36722dd67164c8b4c82da6fa5520fc05be49
John Carter <john.a.carter@capgemini.com>
2018-01-02 16:18:40 +0000

    push to docker hub

--- docker/build-container.bash
+++ docker/build-container.bash
@@ -2,6 +2,7 @@
 
 STATUS=0
 D_TAG=$(git rev-parse --short ${GIT_COMMIT})
-docker build -t hello-node:${D_TAG} . || STATUS=1
-
-
+docker build -t hello-node:${D_TAG} . | tee build.info || STATUS=1
+IMAGE_ID=$(grep 'Successfully built' build.info | awk '{print $NF}')
+docker tag ${IMAGE_ID} johncartercap/hello-node:${D_TAG}
+docker push johncartercap/hello-node
