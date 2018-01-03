cf95374a9aef7f433c22333773e6ce83936f47f4
John Carter <john.a.carter@capgemini.com>
2018-01-03 16:42:02 +0000

    test container id

--- docker/build_and_test_container.bash
+++ docker/build_and_test_container.bash
@@ -11,7 +11,9 @@ IMAGE_ID=$(grep 'Successfully built' build.info | awk '{print $NF}')
 echo "Testing Image hello-node:${GIT_COMMIT}"
 docker run hello-node:${GIT_COMMIT} &
 echo "Getting CONTAINER ID of hello-node:${GIT_COMMIT}"
-CONTAINER_ID=$(docker ps | grep "hello-node:${GIT_COMMIT}" | awk '{print $1}')
+CONTAINER_ID=$(DOCKER_HOST='tcp://192.168.99.101:2375' docker ps | grep "hello-node:${GIT_COMMIT}" | awk '{print $1}')
+echo "CONTAINER_ID = ${CONTAINER_ID}"
+exit 1
 inspec exec test/integration/docker -t docker://${CONTAINER_ID} || STATUS=1
 echo "Stopping the running container hello-node:${GIT_COMMIT}"
 docker kill ${CONTAINER_ID}
