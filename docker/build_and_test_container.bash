246a90124450b6853a02fc4abc4323c84a79cf38
John Carter <john.a.carter@capgemini.com>
2018-01-03 16:28:02 +0000

    get container ID

--- docker/build_and_test_container.bash
+++ docker/build_and_test_container.bash
@@ -7,12 +7,15 @@ echo getting images
 docker images
 #D_TAG=$(git rev-parse --short ${GIT_COMMIT})
 docker build -t hello-node:${GIT_COMMIT} . | tee build.info || STATUS=1
-IMAGE_ID=$(grep 'Successfully built' build.info | awk '{print $NF}')A
+IMAGE_ID=$(grep 'Successfully built' build.info | awk '{print $NF}')
 echo "Testing Image hello-node:${GIT_COMMIT}"
 docker run hello-node:${GIT_COMMIT} &
-inspec exec test/integration/docker -t docker://hello-node:${GIT_COMMIT} || STATUS=1
+echo "Getting CONTAINER ID of hello-node:${GIT_COMMIT}"
+CONTAINER_ID=$(docker ps | grep "hello-node:${GIT_COMMIT}" | awk '{print $1}')
+inspec exec test/integration/docker -t docker://${CONTAINER_ID} || STATUS=1
 echo "Stopping the running container hello-node:${GIT_COMMIT}"
-kill -9 $(ps -aef | grep "hello-node:${GIT_COMMIT}" | grep -v 'grep' | awk '{print $2'})
+docker kill ${CONTAINER_ID}
+# kill -9 $(ps -aef | grep "hello-node:${GIT_COMMIT}" | grep -v 'grep' | awk '{print $2'})
 
 #docker tag ${IMAGE_ID} johncartercap/hello-node:${GIT_COMMIT}
 #docker push johncartercap/hello-node
