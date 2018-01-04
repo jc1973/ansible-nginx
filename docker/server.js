6d01b0f12c3be2c5aa184ed98dfe7f9f212db95e
John Carter <john.a.carter@capgemini.com>
2018-01-04 15:19:03 +0000

    changed text

--- docker/server.js
+++ docker/server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello. Check if deployments are working.');
+  response.end('Hello. Check if build and tests workd and get deployed to K8s cluster.');
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
