c0ad147274861ea05e21a8b17bcbde1121bb8f82
John Carter <john.a.carter@capgemini.com>
2018-01-04 10:24:17 +0000

    path to tests fixed

--- docker/build_and_test_container.bash
+++ docker/build_and_test_container.bash
@@ -13,14 +13,12 @@ docker run hello-node:${GIT_COMMIT} &
 echo "Waiting for container to become available"
 while true; do 
   CONTAINER_ID=$(DOCKER_HOST='tcp://192.168.99.101:2375' docker ps | grep "hello-node:${GIT_COMMIT}" | awk '{print $1}')
-  #curl localhost:8080
   [ ! -z ${CONTAINER_ID} ] && break
   sleep 1 
 done
 echo "Retrieved  CONTAINER ID of hello-node:${GIT_COMMIT}"
-#CONTAINER_ID=$(DOCKER_HOST='tcp://192.168.99.101:2375' docker ps | grep "hello-node:${GIT_COMMIT}" | awk '{print $1}')
 echo "CONTAINER_ID = ${CONTAINER_ID}"
-inspec exec test/integration/docker -t docker://${CONTAINER_ID} || STATUS=1
+inspec exec test/integration/docker/inspec --no-colour -t docker://${CONTAINER_ID} || STATUS=1
 echo "Stopping the running container hello-node:${GIT_COMMIT}"
 docker kill ${CONTAINER_ID}
 exit 1
