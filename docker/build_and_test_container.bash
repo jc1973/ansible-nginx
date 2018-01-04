f55601cdcf288c0eecf762f2ad38cf7dcfd28cd0
John Carter <john.a.carter@capgemini.com>
2018-01-04 09:57:49 +0000

    run inspec tests

--- docker/build_and_test_container.bash
+++ docker/build_and_test_container.bash
@@ -20,10 +20,10 @@ done
 echo "Retrieved  CONTAINER ID of hello-node:${GIT_COMMIT}"
 #CONTAINER_ID=$(DOCKER_HOST='tcp://192.168.99.101:2375' docker ps | grep "hello-node:${GIT_COMMIT}" | awk '{print $1}')
 echo "CONTAINER_ID = ${CONTAINER_ID}"
-exit 1
 inspec exec test/integration/docker -t docker://${CONTAINER_ID} || STATUS=1
 echo "Stopping the running container hello-node:${GIT_COMMIT}"
 docker kill ${CONTAINER_ID}
+exit 1
 # kill -9 $(ps -aef | grep "hello-node:${GIT_COMMIT}" | grep -v 'grep' | awk '{print $2'})
 
 #docker tag ${IMAGE_ID} johncartercap/hello-node:${GIT_COMMIT}
