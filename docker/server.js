baf01d32ad624c22ce4ad5a4bbbb4604e1648e9f
John Carter <john.a.carter@capgemini.com>
2018-01-04 17:05:43 +0000

    different output

--- docker/server.js
+++ docker/server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello. Check if build and tests workd and get deployed to K8s cluster.');
+  response.end('Hello. Check if build and tests pass, then get deployed to K8s cluster.');
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
